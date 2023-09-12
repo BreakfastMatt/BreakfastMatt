048ff5c61c6a7e46e4425211ddff95aa42a5491a
Matthew Lewis <31037009+BreakfastMatt@users.noreply.github.com>
2023-09-12 21:31:13 +0200

    Update gather_stats.js

--- gather_stats.js
+++ gather_stats.js
@@ -6,6 +6,26 @@ if (!accessToken) console.log(`GitHub access token is not defined.`);
 const { Octokit } = require('@octokit/rest');
 const octokit = new Octokit({ auth: accessToken });
 
+// Get the list of all repositories for the user, including private ones
+const fetchRepositoryDetails = async () => {
+    // Get the list of all repositories for the user, including private ones
+    const { data: allRepos } = await octokit.repos.listForAuthenticatedUser();
+
+    // Fetch the repository-level statistics
+    const repoStatsPromises = allRepos.map(async (repo) => {
+      const { data: repoStats } = await octokit.repos.getContributorsStats({ owner: repo.owner.login, repo: repo.name });
+      const mappedRepoDetail = { name: repo.name, owner: repo.owner.login, statistics: repoStats };
+      return mappedRepoDetail;
+    });
+    const repositoryDetails = await Promise.all(repoStatsPromises);
+    
+    // Log repository details
+    console.log("\nRepositories:");
+    repositoryDetails.forEach(repo => console.log(`* ${repo.name}`));
+    console.log(`Total repositories = ${repositoryDetails.length}\n`);
+    return repositoryDetails;
+};
+
 // Fetch various repository statistics for the specified user
 const gatherStatsForUser = async () => {
   try {
@@ -20,30 +40,3 @@ gatherStatsForUser().catch((error) => {
   console.error('Error:', error);
   process.exit(1);
 });
-
-// Get the list of all repositories for the user, including private ones
-const fetchRepositoryDetails = async () => {
-    // Get the list of all repositories for the user, including private ones
-    const { data: allRepos } = await octokit.repos.listForAuthenticatedUser();
-
-    // Fetch the repository-level statistics
-    const repoStatsPromises = allRepos.map(async (repo) => {
-      const { data: repoStats } = await octokit.repos.getContributorsStats({ owner: repo.owner.login, repo: repo.name });
-      const mappedRepoDetail = { name: repo.name, owner: repo.owner.login, statistics: repoStats };
-      return mappedRepoDetail;
-    });
-
-    // Wait for all promises to resolve
-    const repositoryDetails = await Promise.all(repoStatsPromises);
-    
-    // Log repository details
-    logRepositoryBasicDetails(repositoryDetails);
-    return repositoryDetails;
-};
-
-// Log basic repository details (name, total count etc.)
-const logRepositoryBasicDetails = (repositoryDetails) => {
-  console.log("\nRepositories:");
-  repositoryDetails.forEach(repo => console.log(`* ${repo.name}`));
-  console.log(`Total repositories = ${repositoryDetails.length}\n`);
-};
