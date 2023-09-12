4101ccd87bde07acb362cd1143e52ebef1e639ce
Matthew Lewis <31037009+BreakfastMatt@users.noreply.github.com>
2023-09-12 21:23:54 +0200

    Update gather_stats.js

--- gather_stats.js
+++ gather_stats.js
@@ -9,9 +9,8 @@ const octokit = new Octokit({ auth: accessToken });
 // Fetch various repository statistics for the specified user
 const gatherStatsForUser = async () => {
   try {
-    // Get the list of all repositories for the user, including private ones
-    const { data } = await octokit.repos.listForAuthenticatedUser();
-    const repositoryDetails = data.map(repo => ({ name: repo.name, owner: repo.owner.login }));
+    // Get the repository details for the user (public & private)
+    const repositoryDetails = await fetchRepositoryDetails();
 
     // Log repository details
     logRepositoryBasicDetails(repositoryDetails);
@@ -25,9 +24,26 @@ gatherStatsForUser().catch((error) => {
   process.exit(1);
 });
 
+// Get the list of all repositories for the user, including private ones
+const fetchRepositoryDetails = async () => {
+    // Get the list of all repositories for the user, including private ones
+    const { data: allRepos } = await octokit.repos.listForAuthenticatedUser();
+
+    // Fetch the repository-level statistics
+    const repoStatsPromises = allRepos.map(async (repo) => {
+      const { data: repoStats } = await octokit.repos.getStatsContributors({ owner: repo.owner.login, repo: repo.name });
+      const mappedRepoDetail = { name: repo.name, owner: repo.owner.login, statistics: repoStats };
+      return mappedRepoDetail;
+    });
+
+    // Wait for all promises to resolve
+    const repositoryDetails = await Promise.all(repoStatsPromises);
+    return repositoryDetails;
+};
+
 // Log basic repository details (name, total count etc.)
 const logRepositoryBasicDetails = (repositoryDetails) => {
   console.log("\nRepositories:");
   repositoryDetails.forEach(repo => console.log(`* ${repo.name}`));
   console.log(`Total repositories = ${repositoryDetails.length}\n`);
-}
+};
