988e0ced5df1d33a16f2d5b97a241a2b8fe0f4be
Matthew <16mdlewis@gmail.com>
2023-09-13 22:45:24 +0200

    Added another logging function

--- Services/log.js
+++ Services/log.js
@@ -8,3 +8,12 @@ export const log = (...logDetail) => {
     // Log all of the details
     console.log(logDetail);
 }
+
+export const logAndReturn = (detail, description = "") => {
+    // Short-circuit if logging is disabled
+    if (!enableLogging) return detail;
+
+    // Log the detail and return it
+    console.log(`${description}${detail}`);
+    return detail;
+}
